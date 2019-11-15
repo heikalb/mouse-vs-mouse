05ccf2195314d0ca9fe537fd662e6c82c470ed4e
heikalb <heikal93@gmail.com>
2019-11-14 20:41:31 -0800

    documentation

--- classify.py
+++ classify.py
@@ -1,5 +1,6 @@
 """
-
+Classify texts containing the word 'mouse' whether they refer to a mouse the
+animal or the computer mouse.
 Heikal Badrulhisham, 2019 <heikal93@gmail.com>
 """
 import csv
@@ -10,6 +11,11 @@ from sklearn.naive_bayes import MultinomialNB
 
 
 def get_data():
+    """
+    Get text data from .csv files. Associate each text with a label ('animal'
+    vs. 'computer')
+    :return: list of texts, list of labels
+    """
     data = []
 
     for label in ['animal', 'computer']:
@@ -25,23 +31,41 @@ def get_data():
 
 
 def get_vectors(texts):
+    """
+    Derive feature vectors from texts, using Term Frequency times inverse
+    document frequency (TFIDF).
+    :param texts: list of texts
+    :return: list of TFIDF vectors
+    """
     vectorizer = CountVectorizer()
     count_vectors = vectorizer.fit_transform(texts)
     transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(count_vectors)
 
-    return transformer.fit_transform(count_vectors)
+    return tfidf
 
 
 def run_test(classifier, tfidf, labels):
+    """
+    Get predictions of classifier, display its accuracy.
+    :param classifier: a Naive Bayes classifier
+    :param tfidf: list of TFIDF feature vectors
+    :param labels: list of correct labels to predict
+    """
+    # Make predictions
     pred = classifier.predict(tfidf)
 
     num_correct = 0
+
+    # Display actual and predicted labels
     for i in range(len(pred)):
         print(f'Actual: {pred[i]}\tPrediction: {labels[i]}')
 
+        # Tally correct predictions
         if pred[i] == labels[i]:
             num_correct += 1
 
+    # Display accuracy
     proportion = f'{num_correct}/{len(pred)}'
     percentage = round((num_correct/len(pred))*100, 2)
     print(f'Accuracy: {proportion}, {percentage}%')
@@ -49,6 +73,8 @@ def run_test(classifier, tfidf, labels):
 
 def main():
     """
+    Get data, derive feature vectors, train a Naive Bayes classifier, test
+    said classifier.
     """
     # Get data
     texts, labels = get_data()
